7efc0c8280144cdaa7af42bde899062caaead2ec
pnmichalakis <pnmichalakis@gmail.com>
2015-01-07 17:46:46 -0500

    adds twitter api search functionality on backend, pulls trending topics from twitter

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require 'bundler'
 Bundler.require
 
 require 'dotenv'
+Dotenv.load
 
 get '/' do
+	client = Twitter::REST::Client.new do |config|
+  	config.consumer_key        = ENV['CONSUMERKEY']
+  	config.consumer_secret     = ENV['CONSUMERSECRET']
+  	config.access_token        = ENV['ACCESSTOKEN']
+  	config.access_token_secret = ENV['ACCESSTOKENSECRET']
+	end
+	trends = client.get("https://api.twitter.com/1.1/trends/place.json?id=1")
 	erb :index
 end
