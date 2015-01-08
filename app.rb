fe21a4258aca49b3a6c734f6b08740866e0a6a3b
pnmichalakis <pnmichalakis@gmail.com>
2015-01-08 17:19:23 -0500

    reduces twitter trending topics to name of trending topic only

--- app.rb
+++ app.rb
@@ -12,5 +12,9 @@ get '/' do
   	config.access_token_secret = ENV['ACCESSTOKENSECRET']
 	end
 	trends = client.get("https://api.twitter.com/1.1/trends/place.json?id=1")
+	binding.pry
+	trends_name_array = trends.first[:trends].map do |trend|
+												trend[:name]
+											end
 	erb :index
 end
