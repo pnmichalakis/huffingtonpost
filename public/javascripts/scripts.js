e6e6438eec6cc3edd779fe943f2284d3c84b0854
pnmichalakis <pnmichalakis@gmail.com>
2015-01-09 02:47:37 -0500

    selected value in dropdown now outputs alert on screen of that value

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,1 +1,35 @@
 console.log("Hello")
+
+$(document).ready(function(){
+
+// $('#selectorbutton').click(function(){
+// 	console.log("jam")
+// 	$('#selectorform').submit();
+// });
+
+// $("#selectorform").submit(function(e){
+// 	e.preventDefault();
+// 	console.log("throop")
+// });
+
+$("#sections").on('change', function(){
+	 alert($('option:selected').text());
+});
+
+
+
+});
+
+$(function(){
+
+$('#selectorbutton').click(function(){
+	$('#selectorform').submit();
+});
+
+$("#selectorform").submit(function(){
+	console.log("throop")
+});
+
+
+
+});
