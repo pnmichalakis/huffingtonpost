dac9343f96ce12fc4071a0e8fc9ab414ab499422
pnmichalakis <pnmichalakis@gmail.com>
2015-01-09 04:41:25 -0500

    sections to search now accessible in array

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,9 +1,16 @@
 console.log("Hello")
 
+var toSearch = []
+
 $(document).ready(function(){
 
+
 	$("#sections").on('change', function(){
-		$("#selected").append("<li class='selectedsection'>"+ $('option:selected').text() +"</li>")
+		$("#selected").append("<li class='selectedsection'>"+ $('option:selected').text() +"</li>");
+		var option_all = $("option:selected").map(function() {
+			return $(this).text();
+		}).get().join();
+		toSearch.push(option_all);
 	});
 
 });
