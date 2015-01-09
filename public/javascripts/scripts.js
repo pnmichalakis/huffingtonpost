026e590257431df83e6e852b88ef615d9d6f5b75
pnmichalakis <pnmichalakis@gmail.com>
2015-01-09 03:04:09 -0500

    removes unnecessary/redundant code, minor CSS addition

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -2,34 +2,9 @@ console.log("Hello")
 
 $(document).ready(function(){
 
-// $('#selectorbutton').click(function(){
-// 	console.log("jam")
-// 	$('#selectorform').submit();
-// });
-
-// $("#selectorform").submit(function(e){
-// 	e.preventDefault();
-// 	console.log("throop")
-// });
-
-$("#sections").on('change', function(){
-	 alert($('option:selected').text());
-});
-
-
+	$("#sections").on('change', function(){
+		$("#selected").append("<li>"+ $('option:selected').text() +"</li>")
+	});
 
 });
 
-$(function(){
-
-$('#selectorbutton').click(function(){
-	$('#selectorform').submit();
-});
-
-$("#selectorform").submit(function(){
-	console.log("throop")
-});
-
-
-
-});
