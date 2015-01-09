58469a6fd93c1262482e4b89fc0bf89a982921a2
pnmichalakis <pnmichalakis@gmail.com>
2015-01-09 05:17:00 -0500

    dropdown and search now clear entirely when new search is performed

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -4,13 +4,20 @@ var toSearch = []
 
 $(document).ready(function(){
 
-
+	$("#searchtitle").hide();
 	$("#sections").on('change', function(){
+		$("#searchtitle").show();
 		$("#selected").append("<li class='selectedsection'>"+ $('option:selected').text() +"</li>");
 		var option_all = $("option:selected").map(function() {
 			return $(this).text();
 		}).get().join();
 		toSearch.push(option_all);
+	});
+
+	$("#huffposearch").click(function(){
+		$(".selectedsection").empty();
+		console.log(toSearch);
+		toSearch = []
 	});
 
 });
